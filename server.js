acc62eb80630ad5dc8c196dbae347aaff3dbf535
Mustafa Cagri <86930057+salvagedscrap@users.noreply.github.com>
2022-09-02 11:02:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/api/:name', (request,response)=>{
     }
 })
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server is on fire yo at ${PORT}! `);
 } )
